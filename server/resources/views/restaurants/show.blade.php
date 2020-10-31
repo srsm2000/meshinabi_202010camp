58790919ac54f0adfc55f476f7c6004f05fc7a6a
srsm2000 <srsm2000.abv@gmail.com>
2020-10-31 11:37:28 +0900

    views.appのリファクタリング

--- server/resources/views/restaurants/show.blade.php
+++ server/resources/views/restaurants/show.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>めしナビ - 詳細画面</title>
-</head>
-<body>
-    <table>
+@extends('layouts.app')
+
+@section('title', '詳細画面')
+
+@section('content')
+    <table class="table-bordered mb-5 mt-3">
+        {{-- mbはマージンボトム、mtはmargin top --}}
+        <colgroup span="1" style="width:200px;background-color:#efefef;"></colgroup>
+        {{-- colgroupはカラムを設定するときに使う。 --}}
         <tbody>
             <tr>
                 <th>店名</th>
@@ -35,7 +34,7 @@
         </tbody>
     </table>
 
-    <a href="{{ action('RestaurantController@index') }}">戻る</a>    
+    <a href="{{ action('RestaurantController@index') }}">戻る</a>
     {{-- urlをコントローラーのアクションで指定できる --}}
-</body>
-</html>
+
+@endsection
